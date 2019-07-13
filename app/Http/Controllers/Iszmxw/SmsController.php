896679f1ab1bda6e278d8a38f3eaca27d1a5073c
iszmxw <mail@54zm.com>
2019-07-13 17:05:00 +0800

    master

--- app/Http/Controllers/Iszmxw/SmsController.php
+++ app/Http/Controllers/Iszmxw/SmsController.php
@@ -8,10 +8,20 @@ use App\Http\Controllers\Controller;
 
 class SmsController extends Controller
 {
+    public static function url($mobile)
+    {
+        $url = "https://gc.moscales.com/frontend/sms/send?phoneNumber={$mobile}&type=1";
+        return $url;
+    }
+
     //
     public function send(Request $request)
     {
         $data = Sms::getOne();
         dd($data);
+        $url = self::url($data['mobile']);
+        $client = new \Client();
+        $res = $client->get($url)->getBody()->getContents();
+        return $res;
     }
 }
