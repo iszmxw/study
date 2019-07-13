657bccf63860c673e4ed7ebfb9429d266b8e605c
iszmxw <mail@54zm.com>
2019-07-13 11:49:19 +0800

    master

---
+++ public/git_pull.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: iszmxw
+ * Date: 2019/4/28
+ * Time: 17:16
+ */
+
+exec("cd /home/wwwroot/study_54zm_com/ && git pull origin master",$res);
+var_dump($res);
+
