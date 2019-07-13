3344c2c0baf090d64b5599e341f0477c847cca7c
iszmxw <mail@54zm.com>
2019-07-13 16:49:36 +0800

    master

--- routes/web.php
+++ routes/web.php
@@ -20,5 +20,5 @@ Route::get('asas', function () {
 });
 
 Route::group(['prefix' => 'iszmxw'], function () {
-    Route::any('send', 'Iszmxw\Smscontroller@send');
+    Route::any('send', 'Iszmxw\SmsController@send');
 });
