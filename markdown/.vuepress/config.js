a8b1f10ad9df5e87e30420756a6d1371fc504884
追梦小窝 <mail@54zm.com>
2020-08-28 22:01:46 +0800

    更新文档

--- markdown/.vuepress/config.js
+++ markdown/.vuepress/config.js
@@ -6,7 +6,7 @@ module.exports = {
     description: '小窝笔记',
     base: '/',
     port: '',
-    dest: '.vuepress/dist',
+    dest: 'docs',
     head: [
         ['link', { rel: 'icon', href: '/assets/img/logo.png' }],
         ['meta', { 'name': 'viewport', content: "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" }],
